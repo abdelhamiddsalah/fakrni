72d5b35301b3765178980a922ccae039877d47ed
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2025-04-12 10:59:20 +0200

    add splash screen

--- lib/constants/images.dart
+++ lib/constants/images.dart
@@ -6,6 +6,10 @@ class Assets {
   /// assets/images/boy.png
   static const String imagesBoy = "assets/images/boy.png";
 
+  /// Assets for imagesFakr2
+  /// assets/images/fakr2.png
+  static const String imagesFakr2 = "assets/images/fakr2.png";
+
   /// Assets for imagesFather
   /// assets/images/father.png
   static const String imagesFather = "assets/images/father.png";
@@ -13,5 +17,9 @@ class Assets {
   /// Assets for imagesImageInonboarding
   /// assets/images/imageInonboarding.png
   static const String imagesImageInonboarding = "assets/images/imageInonboarding.png";
+
+  /// Assets for imagesModifysplashimage2
+  /// assets/images/modifysplashimage2.png
+  static const String imagesModifysplashimage2 = "assets/images/modifysplashimage2.png";
 }
 
